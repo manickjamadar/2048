610682e67d0c9a7696a9e78b59927e2ae75ebadf
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-08 09:03:46 +0530

    created basic home screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('2048'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
