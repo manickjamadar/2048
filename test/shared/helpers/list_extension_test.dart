c6e58b3b3de5e932b1787b66b7f26621dd4d2c0a
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-12 16:03:33 +0530

    list extensions moves to shared/helpers

--- test/shared/helpers/list_extension_test.dart
+++ test/shared/helpers/list_extension_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import "package:twozerofoureight/helpers/list_extensions.dart";
+import "package:twozerofoureight/shared/helpers/list_extensions.dart";
 
 void main() {
   group("List Extensions test => ", () {
