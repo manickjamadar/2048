a29651ace3b7ef0e1507986b2ef2afabead63e5b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-12 16:13:33 +0530

    uniqued id value object created

---
+++ lib/domain/core/value_objects/unique_id.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+class UniqueId extends Equatable {
+  final String value;
+
+  const UniqueId._(this.value);
+
+  factory UniqueId() {
+    return UniqueId._(Uuid().v4());
+  }
+
+  factory UniqueId.fromString(String str) {
+    return UniqueId._(str);
+  }
+
+  @override
+  List<Object> get props => [value];
+}
