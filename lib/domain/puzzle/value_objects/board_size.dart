79730dc37760f44572e0f76ba24f68822fe87a04
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-12 16:23:21 +0530

    board size value object created

---
+++ lib/domain/puzzle/value_objects/board_size.dart
@@ -0,0 +1,11 @@
+import 'package:equatable/equatable.dart';
+
+class BoardSize extends Equatable {
+  final int value;
+  const BoardSize(this.value) : assert(value > 2);
+
+  int get totalSize => value * value;
+
+  @override
+  List<Object> get props => [value];
+}
