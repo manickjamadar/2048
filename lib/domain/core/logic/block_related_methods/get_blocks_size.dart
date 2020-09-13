dd717480f36144b7e4ce8633c2ca19f187f47348
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-13 12:52:06 +0530

    added documentation in get blocks size methods

--- lib/domain/core/logic/block_related_methods/get_blocks_size.dart
+++ lib/domain/core/logic/block_related_methods/get_blocks_size.dart
@@ -4,6 +4,8 @@ import 'package:dartz/dartz.dart';
 import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
 import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
 
+///return boardsize if length of blocks list is matched with blocks index size's total size and every blocks index size should be the same size ,
+/// otherwise it will return none;
 Option<BoardSize> getBlocksSize(List<Block> blocks) {
   int sizeValue = sqrt(blocks.length).truncate().toInt();
   for (var i = 0; i < blocks.length; i++) {
