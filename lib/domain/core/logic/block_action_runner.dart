77dff61f43410285b6a9363bc5c6acb604889fae
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-13 13:32:04 +0530

    block action runner class created

---
+++ lib/domain/core/logic/block_action_runner.dart
@@ -0,0 +1,12 @@
+import 'package:twozerofoureight/domain/core/logic/block_actors/block_actor.dart';
+import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
+
+///Runner responsibility is to run and give back the blocks which is modified by actors
+class BlockActionRunner {
+  final BlockActor actor;
+
+  const BlockActionRunner(this.actor);
+  List<Block> run() {
+    return actor.act();
+  }
+}
