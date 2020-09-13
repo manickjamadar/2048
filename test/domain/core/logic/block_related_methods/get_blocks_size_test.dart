1b7e65d309b2bc6b47d83633e239ec3f374a2b86
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-13 12:50:08 +0530

    get blocks size test file created

---
+++ test/domain/core/logic/block_related_methods/get_blocks_size_test.dart
@@ -0,0 +1,74 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:twozerofoureight/domain/core/logic/block_related_methods/get_blocks_size.dart';
+import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
+import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
+
+void main() {
+  group("Get empty blocks method test => ", () {
+    test("should return none if list length is not right", () {
+      final blocks = [
+        Block.random(index: 3, boardSize: 4),
+        Block.random(index: 4, boardSize: 4),
+        Block.random(index: 5, boardSize: 4),
+        Block.random(index: 6, boardSize: 4),
+        Block.random(index: 8, boardSize: 4),
+      ];
+      expect(getBlocksSize(blocks), equals(None()));
+    });
+    test("should return none for correct list length but differnt blocks sizes",
+        () {
+      final blocks = [
+        Block.random(index: 0, boardSize: 3),
+        Block.random(index: 1, boardSize: 3),
+        Block.random(index: 2, boardSize: 3),
+        Block.random(index: 3, boardSize: 3),
+        Block.random(index: 4, boardSize: 3),
+        Block.random(index: 5, boardSize: 3),
+        Block.random(index: 6, boardSize: 4),
+        Block.random(index: 7, boardSize: 3),
+        Block.random(index: 8, boardSize: 4),
+        Block.random(index: 9, boardSize: 4),
+      ];
+
+      expect(getBlocksSize(blocks), equals(None()));
+    });
+
+    test(
+        "should return none form incorrect list length and for every boardsize 4",
+        () {
+      final blocks = [
+        Block.random(index: 0, boardSize: 4),
+        Block.random(index: 1, boardSize: 4),
+        Block.random(index: 2, boardSize: 4),
+        Block.random(index: 3, boardSize: 4),
+        Block.random(index: 4, boardSize: 4),
+        Block.random(index: 5, boardSize: 4),
+        Block.random(index: 6, boardSize: 4),
+        Block.random(index: 7, boardSize: 4),
+        Block.random(index: 8, boardSize: 4),
+        Block.random(index: 9, boardSize: 4),
+      ];
+
+      expect(getBlocksSize(blocks), equals(None()));
+    });
+
+    test("should return correct size for correct list length and board size 3",
+        () {
+      final blocks = [
+        Block.random(index: 0, boardSize: 3),
+        Block.random(index: 1, boardSize: 3),
+        Block.random(index: 2, boardSize: 3),
+        Block.random(index: 3, boardSize: 3),
+        Block.random(index: 4, boardSize: 3),
+        Block.random(index: 5, boardSize: 3),
+        Block.random(index: 6, boardSize: 3),
+        Block.random(index: 7, boardSize: 3),
+        Block.random(index: 8, boardSize: 3),
+      ];
+      final expectedBoardSize = BoardSize(3);
+
+      expect(getBlocksSize(blocks), equals(Some(expectedBoardSize)));
+    });
+  });
+}
