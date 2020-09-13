88377ea0e0b8de7d2231bcb4bbb3168fec241647
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-13 17:15:41 +0530

    refactored get blocks size method with is board form method

--- lib/domain/core/logic/block_related_methods/get_blocks_size.dart
+++ lib/domain/core/logic/block_related_methods/get_blocks_size.dart
@@ -1,18 +1,14 @@
-import 'dart:math';
-
 import 'package:dartz/dartz.dart';
+import 'package:twozerofoureight/domain/core/logic/block_related_methods/is_board_form.dart';
 import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
 import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
 
 ///return boardsize if length of blocks list is matched with blocks index size's total size and every blocks index size should be the same size ,
 /// otherwise it will return none;
 Option<BoardSize> getBlocksSize(List<Block> blocks) {
-  int sizeValue = sqrt(blocks.length).truncate().toInt();
-  for (var i = 0; i < blocks.length; i++) {
-    final block = blocks[i];
-    if (block.index.size.value != sizeValue) {
-      return None();
-    }
+  if (isBoardForm(blocks)) {
+    return Some(BoardSize(blocks[0].index.size.value));
+  } else {
+    return None();
   }
-  return Some(BoardSize(sizeValue));
 }
