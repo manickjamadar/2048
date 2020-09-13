d7c6ceb57f73b03db51a6a550fbdcdfbefb0c587
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-13 15:26:09 +0530

    makes generate empty blocks growable false

--- lib/domain/core/logic/block_related_methods/generate_empty_blocks.dart
+++ lib/domain/core/logic/block_related_methods/generate_empty_blocks.dart
@@ -3,5 +3,6 @@ import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
 
 List<Block> generateEmptyBlocks(BoardSize size) {
   return List<Block>.generate(size.totalSize,
-      (index) => Block.empty(index: index, boardSize: size.value));
+      (index) => Block.empty(index: index, boardSize: size.value),
+      growable: false);
 }
