85c4442ff69d3306a3eafd97b6d7c0204912fb62
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-18 11:56:58 +0530

    board slidable getter created

--- lib/domain/puzzle/models/board/board.dart
+++ lib/domain/puzzle/models/board/board.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:twozerofoureight/domain/core/logic/action_runner/action_runner.dart';
 import 'package:twozerofoureight/domain/core/logic/block_related_methods/generate_empty_blocks.dart';
 import 'package:twozerofoureight/domain/core/logic/block_related_methods/get_blocks_size.dart';
 import 'package:twozerofoureight/domain/core/logic/block_related_methods/is_board_form.dart';
+import 'package:twozerofoureight/domain/core/logic/board_actors/slide_actor.dart';
+import 'package:twozerofoureight/domain/core/logic/board_direction.dart';
 import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
 import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
 part 'board.freezed.dart';
@@ -19,4 +22,25 @@ abstract class Board implements _$Board {
   bool get isValid => isBoardForm(blocks);
 
   BoardSize get size => getBlocksSize(blocks).fold(() => null, id);
+
+  bool get slidable {
+    if (!isValid) {
+      return false;
+    }
+    final allDirection = [
+      BoardDirection.up(),
+      BoardDirection.down(),
+      BoardDirection.left(),
+      BoardDirection.right(),
+    ];
+    for (var i = 0; i < allDirection.length; i++) {
+      final direction = allDirection[i];
+      final slidedBoard =
+          ActionRunner(SlideActor(this, direction: direction)).run();
+      if (slidedBoard != this) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
