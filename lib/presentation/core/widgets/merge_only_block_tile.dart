2495fee958c952be2a91ba6e18f433a434ef7515
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-20 15:47:27 +0530

    merge only block tile widget created

---
+++ lib/presentation/core/widgets/merge_only_block_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
+import 'package:twozerofoureight/presentation/core/helpers/get_tile_position.dart';
+import 'package:twozerofoureight/presentation/core/widgets/pulse_transition.dart';
+import 'package:twozerofoureight/presentation/core/widgets/tile.dart';
+
+class MergeOnlyBlockTile extends StatelessWidget {
+  final Tile tile;
+  final Block block;
+  final Animation<double> pulse;
+
+  const MergeOnlyBlockTile(
+      {Key key,
+      @required this.tile,
+      @required this.block,
+      @required this.pulse})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: block.isEmpty ? 0 : 1,
+      child: Transform.translate(
+        offset: getTilePosition(
+          index: block.index.value,
+          tileSize: tile.size,
+          boardSize: block.index.size,
+        ),
+        child: PulseTransition(
+          child: tile,
+          pulse: pulse,
+        ),
+      ),
+    );
+  }
+}
