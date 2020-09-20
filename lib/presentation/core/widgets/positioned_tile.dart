72a69e69b7eb1cca70a1a864062e330c9da816ed
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-20 15:34:40 +0530

    positioned tile widget created

---
+++ lib/presentation/core/widgets/positioned_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
+import 'package:twozerofoureight/presentation/core/helpers/get_tile_position.dart';
+import 'package:twozerofoureight/presentation/core/widgets/tile.dart';
+
+class PositionedTile extends StatelessWidget {
+  final Tile tile;
+  final int index;
+  final BoardSize boardSize;
+
+  const PositionedTile(
+      {Key key,
+      @required this.tile,
+      @required this.index,
+      @required this.boardSize})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: getTilePosition(
+          index: index, tileSize: tile.size, boardSize: boardSize),
+      child: tile,
+    );
+  }
+}
