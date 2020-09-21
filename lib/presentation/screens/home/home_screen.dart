25f5839f55398eb7f931e320513acf2875ebdedb
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-21 11:19:29 +0530

    basic home screen created

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:twozerofoureight/presentation/core/my_icons.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final style = TextStyle(fontSize: 30, color: Colors.red);
+    final boards = [
+      "Tiny 3x3",
+      "Classic 4x4",
+      "Big 5x5",
+      "Bigger 6x6",
+      "Huge 7x7"
+    ];
     return Scaffold(
-      appBar: AppBar(
-        title: Text("2048"),
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(MyIcons.logo, size: 100),
+          SizedBox(
+              height: 200,
+              child: ListWheelScrollView(
+                controller: FixedExtentScrollController(initialItem: 3),
+                overAndUnderCenterOpacity: 0.2,
+                clipBehavior: Clip.none,
+                onSelectedItemChanged: (value) {
+                  print(value);
+                },
+                itemExtent: 50,
+                children:
+                    boards.map((board) => Text(board, style: style)).toList(),
+              )),
+          RaisedButton(
+            child: Text("Play"),
+            onPressed: () {},
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(MyIcons.colorPallete),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(MyIcons.rating),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(MyIcons.about),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(MyIcons.save),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
       ),
-    );
+    ));
   }
 }
