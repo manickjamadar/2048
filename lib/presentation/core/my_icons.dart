d2a1f3c90ddcad0a598f227c43333174da119456
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-21 10:54:28 +0530

    custom icons added

---
+++ lib/presentation/core/my_icons.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyIcons {
+  MyIcons._();
+
+  static const _kFontFam = 'MyIcons';
+  static const _kFontPkg = null;
+
+  static const IconData logo =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData colorPallete = Icons.color_lens;
+  static const IconData rating = Icons.star;
+  static const IconData about = Icons.info;
+  static const IconData save = Icons.save_alt;
+  static const IconData reset = Icons.cached;
+  static const IconData home = Icons.home;
+  static const IconData undo = Icons.replay;
+  static const IconData music = Icons.volume_up;
+  static const IconData musicOff = Icons.volume_off;
+  static const IconData delete = Icons.delete_forever;
+  static const IconData play = Icons.play_arrow;
+}
