f36306b39eb11fabc94b7b03d463d078140db001
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-23 17:53:59 +0530

    high score manager cubit registered

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import "package:hive_flutter/hive_flutter.dart";
 import 'package:twozerofoureight/application/board_option_cubit/board_option_cubit.dart';
+import 'package:twozerofoureight/domain/high_score_manager/facade/high_score_manager_facade.dart';
 import 'package:twozerofoureight/presentation/core/app.dart';
 import 'package:twozerofoureight/service_locator.dart';
+import 'application/high_score_manager/high_score_manager_cubit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   initServiceLocator();
-  runApp(
-      BlocProvider(create: (_) => locator<BoardOptionCubit>(), child: App()));
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (_) => locator<BoardOptionCubit>(),
+      ),
+      BlocProvider(
+        create: (context) => HighScoreManagerCubit(
+            highScoreManagerFacade: locator<IHighScoreManagerFacade>(),
+            boardOptionCubit: BlocProvider.of<BoardOptionCubit>(context)),
+      )
+    ],
+    child: App(),
+  ));
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,12 +1,20 @@
 import 'package:get_it/get_it.dart';
 import 'package:twozerofoureight/application/board_option_cubit/board_option_cubit.dart';
+import 'package:twozerofoureight/domain/high_score_manager/facade/high_score_manager_facade.dart';
+import 'package:twozerofoureight/infrastructure/high_score_manager/facade/high_score_manager_facade.dart';
 
 final locator = GetIt.I;
 
 void initServiceLocator() {
   initBloc();
+  initFacade();
 }
 
 void initBloc() {
   locator.registerFactory<BoardOptionCubit>(() => BoardOptionCubit());
+}
+
+void initFacade() async {
+  locator.registerLazySingleton<IHighScoreManagerFacade>(
+      () => HighScoreManagerFacade());
 }
