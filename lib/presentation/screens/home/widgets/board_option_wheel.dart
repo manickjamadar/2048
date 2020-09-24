1ae9e971eb54b791a2fb216e72ab316117f0ce9e
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-24 15:06:28 +0530

    board option scorll glow effect fixed

--- lib/presentation/screens/home/widgets/board_option_wheel.dart
+++ lib/presentation/screens/home/widgets/board_option_wheel.dart
@@ -15,23 +15,28 @@ class BoardOptionWheel extends StatelessWidget {
       : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final style = TextStyle(fontSize: 30, color: Colors.red);
-    return ListWheelScrollView(
-      key: ValueKey(isLoading),
-      controller: FixedExtentScrollController(initialItem: currentIndex),
-      overAndUnderCenterOpacity: 0.2,
-      onSelectedItemChanged: (int index) {
-        if (onChange != null) {
-          onChange(index);
-        }
-      },
-      clipBehavior: Clip.none,
-      itemExtent: 50,
-      children: options
-          .map((option) => Text(
-              "${option.title} ${option.size.value} x ${option.size.value}",
-              style: style))
-          .toList(),
-    );
+    final style = TextStyle(fontSize: 30, color: Colors.white);
+    return NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowGlow();
+          return;
+        },
+        child: ListWheelScrollView(
+          key: ValueKey(isLoading),
+          controller: FixedExtentScrollController(initialItem: currentIndex),
+          overAndUnderCenterOpacity: 0.2,
+          onSelectedItemChanged: (int index) {
+            if (onChange != null) {
+              onChange(index);
+            }
+          },
+          clipBehavior: Clip.none,
+          itemExtent: 50,
+          children: options
+              .map((option) => Text(
+                  "${option.title} ${option.size.value} x ${option.size.value}",
+                  style: style))
+              .toList(),
+        ));
   }
 }
