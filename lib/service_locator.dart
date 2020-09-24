ff284022002996b189d7697e04c816deca464c79
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-24 15:47:30 +0530

    theme color facade registered

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -4,8 +4,10 @@ import 'package:twozerofoureight/application/board_option_cubit/board_option_cub
 import 'package:twozerofoureight/application/theme_color/theme_color_cubit.dart';
 import 'package:twozerofoureight/domain/board_option/facade/board_option_facade.dart';
 import 'package:twozerofoureight/domain/high_score_manager/facade/high_score_manager_facade.dart';
+import 'package:twozerofoureight/domain/theme_color/facade/theme_color_facade.dart';
 import 'package:twozerofoureight/infrastructure/board_option/facade/board_option_facade.dart';
 import 'package:twozerofoureight/infrastructure/high_score_manager/facade/high_score_manager_facade.dart';
+import 'package:twozerofoureight/infrastructure/theme_color/facade/theme_color_facade.dart';
 
 final locator = GetIt.I;
 
@@ -17,13 +19,17 @@ Future<void> initServiceLocator() async {
 void initBloc() {
   locator.registerFactory<BoardOptionCubit>(
       () => BoardOptionCubit(boardOptionFacade: locator<IBoardOptionFacade>()));
-  locator.registerFactory<ThemeColorCubit>(() => ThemeColorCubit());
+  locator.registerFactory<ThemeColorCubit>(
+      () => ThemeColorCubit(themeColorFacade: locator<IThemeColorFacade>()));
 }
 
 Future<void> initFacade() async {
   final boardOptionBox = await Hive.openBox<int>("board_option");
+  final themeColorBox = await Hive.openBox<int>("theme_color");
   locator.registerLazySingleton<IHighScoreManagerFacade>(
       () => HighScoreManagerFacade());
   locator.registerLazySingleton<IBoardOptionFacade>(
       () => BoardOptionFacade(boardOptionBox));
+  locator.registerLazySingleton<IThemeColorFacade>(
+      () => ThemeColorFacade(themeColorBox));
 }
