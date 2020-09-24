f754fdf27df3c442cb05d9cd7a8e7ac981bfa235
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-24 15:47:41 +0530

    theme color facade used in cubit

--- lib/application/theme_color/theme_color_cubit.dart
+++ lib/application/theme_color/theme_color_cubit.dart
@@ -1,11 +1,33 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:twozerofoureight/domain/theme_color/models/theme_color.dart';
+import 'package:twozerofoureight/infrastructure/theme_color/facade/theme_color_facade.dart';
 
 part 'theme_color_state.dart';
 part 'theme_color_cubit.freezed.dart';
 
 class ThemeColorCubit extends Cubit<ThemeColorState> {
-  ThemeColorCubit() : super(ThemeColorState.initial());
+  final ThemeColorFacade themeColorFacade;
+  ThemeColorCubit({@required this.themeColorFacade})
+      : super(ThemeColorState.initial()) {
+    _init();
+  }
+
+  void _init() async {
+    final indexOption = await themeColorFacade.getCurrentIndex();
+    final currentIndex = indexOption.fold((l) => 0, id);
+    emit(state.copyWith(currentIndex: currentIndex));
+  }
+
+  //events
+  void change(int index) {
+    if (index < 0 || index >= state.themeColors.length) {
+      return;
+    }
+
+    emit(state.copyWith(currentIndex: index));
+    themeColorFacade.saveIndex(index);
+  }
 }
