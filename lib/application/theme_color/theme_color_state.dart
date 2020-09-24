b06cf00c188ccac88f064a0f6e9b8e5f29bd878b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-24 15:00:10 +0530

    added 5 theme color

--- lib/application/theme_color/theme_color_state.dart
+++ lib/application/theme_color/theme_color_state.dart
@@ -8,8 +8,30 @@ abstract class ThemeColorState implements _$ThemeColorState {
       @required List<ThemeColor> themeColors}) = _ThemeColorState;
   factory ThemeColorState.initial() {
     return ThemeColorState(
-        currentIndex: 0,
-        themeColors: [ThemeColor(first: Colors.blue, second: Colors.indigo)]);
+      currentIndex: 0,
+      themeColors: [
+        ThemeColor(
+          first: Color.fromRGBO(87, 194, 208, 1),
+          second: Color.fromRGBO(0, 118, 184, 1),
+        ),
+        ThemeColor(
+          first: Color.fromRGBO(133, 208, 87, 1),
+          second: Color.fromRGBO(0, 184, 184, 1),
+        ),
+        ThemeColor(
+          first: Color.fromRGBO(233, 79, 125, 1),
+          second: Color.fromRGBO(242, 157, 79, 1),
+        ),
+        ThemeColor(
+          first: Color.fromRGBO(211, 79, 233, 1),
+          second: Color.fromRGBO(242, 79, 98, 1),
+        ),
+        ThemeColor(
+          first: Color.fromRGBO(79, 131, 233, 1),
+          second: Color.fromRGBO(170, 79, 242, 1),
+        ),
+      ],
+    );
   }
 
   ThemeColor get currentThemeColor => themeColors[currentIndex];
