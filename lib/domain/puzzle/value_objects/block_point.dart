f91d2607ccd7333dd73296bb8e8a26310bcf1e7b
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-26 12:06:56 +0530

    made block point constructor public

--- lib/domain/puzzle/value_objects/block_point.dart
+++ lib/domain/puzzle/value_objects/block_point.dart
@@ -4,17 +4,17 @@ import 'package:equatable/equatable.dart';
 
 class BlockPoint extends Equatable {
   final int value;
-  const BlockPoint._(this.value);
+  const BlockPoint(this.value);
 
   factory BlockPoint.random() {
     return Random().nextBool() ? BlockPoint.two : BlockPoint.four;
   }
 
-  BlockPoint mergedPoint() => BlockPoint._(this.value + this.value);
+  BlockPoint mergedPoint() => BlockPoint(this.value + this.value);
 
-  static BlockPoint two = BlockPoint._(2);
-  static BlockPoint four = BlockPoint._(4);
-  static BlockPoint empty = BlockPoint._(0);
+  static BlockPoint two = BlockPoint(2);
+  static BlockPoint four = BlockPoint(4);
+  static BlockPoint empty = BlockPoint(0);
 
   @override
   List<Object> get props => [value];
