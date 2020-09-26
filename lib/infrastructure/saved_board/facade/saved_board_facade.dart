8127dd8f2cf36149efa58703ad85ca373e14e5d8
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-26 20:08:43 +0530

    saved board datasource implemented in facade

--- lib/infrastructure/saved_board/facade/saved_board_facade.dart
+++ lib/infrastructure/saved_board/facade/saved_board_facade.dart
@@ -1,42 +1,42 @@
-import 'package:twozerofoureight/domain/board_option/models/board_option.dart';
+import 'package:flutter/foundation.dart';
 import 'package:twozerofoureight/domain/core/value_objects/unique_id.dart';
 import 'package:dartz/dartz.dart';
-import 'package:twozerofoureight/domain/puzzle/models/block/block.dart';
-import 'package:twozerofoureight/domain/puzzle/models/board/board.dart';
-import 'package:twozerofoureight/domain/puzzle/models/puzzle/puzzle.dart';
-import 'package:twozerofoureight/domain/puzzle/value_objects/board_score.dart';
-import 'package:twozerofoureight/domain/puzzle/value_objects/board_size.dart';
+import 'package:twozerofoureight/domain/saved_board/data_source/saved_board_data_source.dart';
 import 'package:twozerofoureight/domain/saved_board/facade/saved_board_facade.dart';
 import 'package:twozerofoureight/domain/saved_board/models/saved_board.dart';
+import 'package:twozerofoureight/infrastructure/saved_board/entities/saved_board_entity.dart';
 
 class SavedBoardFacade extends ISavedBoardFacade {
+  final ISavedBoardDataSource dataSource;
+
+  SavedBoardFacade({@required this.dataSource});
   @override
   Future<Either<Error, Unit>> create(SavedBoard savedBoard) async {
-    // TODO: implement create
-    return Right(unit);
+    try {
+      await dataSource.create(SavedBoardEntity.fromModel(savedBoard));
+      return Right(unit);
+    } catch (_) {
+      return Left(Error());
+    }
   }
 
   @override
   Future<Either<Error, Unit>> delete(UniqueId id) async {
-    // TODO: implement delete
-    return Right(unit);
+    try {
+      await dataSource.delete(id.value);
+      return Right(unit);
+    } catch (_) {
+      return Left(Error());
+    }
   }
 
   @override
   Future<Either<Error, List<SavedBoard>>> findAll() async {
-    // TODO: implement findAll
-    // final board = Board.empty(BoardSize(3));
-    // board.blocks[0] = Block.random(index: 0, boardSize: 3);
-    // board.blocks[1] = Block.random(index: 1, boardSize: 3);
-    // final savedBoard = SavedBoard(
-    //     id: UniqueId(),
-    //     option: BoardOption(size: BoardSize(3), title: "Tiny"),
-    //     puzzle: Puzzle(
-    //       isGameOver: false,
-    //       board: board,
-    //       score: BoardScore(2345),
-    //       previousBoard: None(),
-    //     ));
-    return Right([]);
+    try {
+      final entityList = await dataSource.findAll();
+      return Right(entityList.map((entity) => entity.toModel()).toList());
+    } catch (_) {
+      return Left(Error());
+    }
   }
 }
