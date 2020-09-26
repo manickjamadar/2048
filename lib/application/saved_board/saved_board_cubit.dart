32e02e863f2db654dd2e30671a21b413574ae263
Creanick <creanick@Creanicks-MacBook-Air.local>
2020-09-26 19:13:07 +0530

    added undo event in saved board cubit

--- lib/application/saved_board/saved_board_cubit.dart
+++ lib/application/saved_board/saved_board_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:twozerofoureight/domain/core/value_objects/unique_id.dart';
@@ -10,7 +12,7 @@ part 'saved_board_cubit.freezed.dart';
 class SavedBoardCubit extends Cubit<SavedBoardState> {
   final ISavedBoardFacade savedBoardFacade;
   SavedBoardCubit({@required this.savedBoardFacade}) : super(_Initial());
-
+  List<SavedBoard> _previousBoardList;
   //events
   void init() async {
     final savedBoardListOption = await savedBoardFacade.findAll();
@@ -31,14 +33,25 @@ class SavedBoardCubit extends Cubit<SavedBoardState> {
         });
   }
 
-  void delete(UniqueId id) {
+  void delete(UniqueId id, Future<bool> shouldPersist) async {
     state.maybeWhen(
         orElse: () {},
         loaded: (list) {
+          _previousBoardList = [...list];
           final newList =
               list.where((savedBoard) => savedBoard.id != id).toList();
           emit(SavedBoardState.loaded(savedBoards: newList));
-          savedBoardFacade.delete(id);
+          shouldPersist.then((value) {
+            if (value) {
+              savedBoardFacade.delete(id);
+            }
+          });
         });
+  }
+
+  void undo() {
+    if (_previousBoardList != null) {
+      emit(SavedBoardState.loaded(savedBoards: _previousBoardList));
+    }
   }
 }
